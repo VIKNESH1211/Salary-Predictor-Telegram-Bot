bc96a1f8508e3d0b843e0b15ef54b2133f764491
Viknesh <vikneshselva1@gmail.com>
2025-04-22 09:01:11 +0530

    prod 1.0 Start Server function

--- bot.py
+++ bot.py
@@ -204,12 +204,16 @@ def fallback_handler(message):
 
 import threading
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  
+
 
 @app.route('/')
 def index():
-    return "Bot is running!"
+    return "Bot is running!", 200
+
 
 def start_bot():
     while True:
@@ -227,4 +231,3 @@ def start_bot():
 if __name__ == "__main__":
     threading.Thread(target=start_bot).start()
     app.run(host="0.0.0.0", port=10000)
-
